f98e220dd16deaa7ce74944cd832d7534bf52bae
Mohammad Umair Mohammad Farooque <106133110+umair-2707@users.noreply.github.com>
2024-08-14 14:52:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const github = require('@actions/github');
 try {
     // 'who-to-greet' input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
-    console.log('Hello ${nameToGreet}!');
+    console.log(`Hello ${nameToGreet}!`);
+    
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // get the json webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.contect.payload, undefined, 2)
-    console.log('the event payload: ${payload}');
-}   catch (error) {
+    
+    // get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
     core.setFailed(error.message);
 }
